d6a3ff32360c56f506a1e418bbd59b76cc815dfe
David Samuel <davidsamuel.7878@gmail.com>
2025-01-08 15:53:52 +1100

    update synthetic data generation script

--- Scripts/generate_speech_to_speech.py
+++ Scripts/generate_speech_to_speech.py
@@ -19,11 +19,19 @@ def load_config(config_path):
         return json.load(f)
 
 def load_metadata(dataset_path):
-    """Load metadata.csv from dataset directory"""
-    metadata_path = os.path.join(dataset_path, "metadata.csv")
-    return pd.read_csv(metadata_path)
+    return pd.read_csv(dataset_path)
 
-def generate_single_audio(text: str, source_style: torch.Tensor, target_style: torch.Tensor, language: str, output_filepath: str, synthesizer: StyleTTS2Synthesizer, **kwargs):
+def s2s(text: str, 
+        language: str,
+        source_style, 
+        target_style, 
+        output_filepath: str, 
+        synthesizer: StyleTTS2Synthesizer, 
+        speaker_config, 
+        max_duration: float = 20.0,
+        fadeout: float = 0.1,
+        silence: float = 0.1,
+        ):
     """Helper function to generate a single audio file"""
     try:
         wav = synthesizer.s2s(
@@ -31,22 +39,33 @@ def generate_single_audio(text: str, source_style: torch.Tensor, target_style: t
             source_style,
             target_style,
             language,
-            alpha=kwargs.get("alpha", 0.1),
-            beta=kwargs.get("beta", 0.3),
-            diffusion_steps=kwargs.get("diffusion_steps", 10),
-            phonemes=kwargs.get("phonemes", False)
+            alpha=speaker_config["alpha"],
+            beta=speaker_config["beta"],
+            diffusion_steps=speaker_config.get("diffusion_steps", 10),
+            phonemes=False
         )
         duration = len(wav) / SAMPLING_RATE
-        if kwargs.get("max_duration", 0) and duration > kwargs.get("max_duration", 0):
-            print(f"Skipping audio with {kwargs['speaker_config']['name']} - Duration {duration:.2f}s exceeds limit of {kwargs.get('max_duration', 0)}s")
+        if max_duration and duration > max_duration:
+            print(f"Skipping audio with {speaker_config['name']} - Duration {duration:.2f}s exceeds limit of {max_duration}s")
             return False
-        fadeout_samples = int(0.030 * SAMPLING_RATE)  # 25ms
+        
+        # Apply fadeout with configurable duration
+        fadeout_samples = int(fadeout * SAMPLING_RATE)
         fadeout_window = np.linspace(1, 0, fadeout_samples)
         wav[-fadeout_samples:] *= fadeout_window
+        
+        # Add configurable silence duration
+        silence_length = int(SAMPLING_RATE * silence)
+        silence = np.zeros(silence_length)
+        wav = np.concatenate([wav, silence])
+        
         sf.write(str(output_filepath), wav, SAMPLING_RATE)
+        # Speed up audio
+        from audiostretchy.stretch import stretch_audio
+        stretch_audio(output_filepath, output_filepath, ratio=0.85)
         return True
     except Exception as e:
-        print(f"Error processing with {kwargs['speaker_config']['name']}: {e}")
+        print(f"Error processing with {output_filepath}: {e}")
         return False
 
 def generate_speech_to_speech(model_config_path, 
@@ -54,13 +73,17 @@ def generate_speech_to_speech(model_config_path,
                               output_path, 
                               speaker_config_path, 
                               max_duration, 
-                              limit):
+                              limit,
+                              fadeout,
+                              silence):
+    
     config = load_config(speaker_config_path)
     settings = config['speakers']
     synthesizer = StyleTTS2Synthesizer(config_path=model_config_path, checkpoint_path=checkpoint_path)
     output_path = Path(output_path)
     if output_path.exists():
         shutil.rmtree(output_path)
+        
     wavs_dir = output_path / "wavs"
     wavs_dir.mkdir(parents=True, exist_ok=True)
     
@@ -72,38 +95,39 @@ def generate_speech_to_speech(model_config_path,
         for idx, row in tqdm(df.iterrows(), total=len(df), desc=f"Processing {speaker_config['name']}"):
             if limit and idx >= limit:
                 break
-            output_filename = row[audio_column]
-            output_filename = output_filename.split("/")[-1]
-            if speaker_config["name"]:
-                output_filename = f"{speaker_config['name']}_{output_filename}"
-            output_filepath = wavs_dir /output_filename
             try:
                 text_column = speaker_config["text_column"]
                 audio_column = speaker_config["audio_column"]
-                reference_path = speaker_config["reference_path"]
-                speaker_id = row["speaker_id"]
                 text = row[text_column]
-                source_audio_path = row[audio_column]
-                source_style = synthesizer.compute_style(source_audio_path)
-                target_style = synthesizer.compute_style(reference_path)
-                language = row["language"]
-                alpha=speaker_config["alpha"]
-                beta=speaker_config["beta"]
-                embedding_scale=speaker_config["embedding_scale"]
-                if generate_single_audio(text, 
-                                        source_style, 
-                                        target_style, 
-                                        language, 
-                                        output_filepath, 
-                                        synthesizer, 
-                                        max_duration=max_duration,
-                                        alpha=alpha,
-                                        beta=beta,
-                                        embedding_scale=embedding_scale,
-                                        speaker_config=speaker_config):
-                    metadata.append(f"{output_filename}|{text}|{speaker_id}")
+                
+                output_filename = text
+                if speaker_config["name"]:
+                    output_filename = f"{speaker_config['name']}_{output_filename}"
+                output_filepath = wavs_dir / f"{output_filename}.wav"
+                
+                path_to_source_audio = Path(speaker_config["root_path"]) / row[audio_column]
+                path_to_reference_audio =  speaker_config["reference_path"]
+                
+                source_style = synthesizer.compute_style(path_to_source_audio)
+                target_style = synthesizer.compute_style(path_to_reference_audio)
+                
+                language = speaker_config["language"]
+                if language == "en-au":
+                    language = "en-gb"
+
+                if s2s(text, 
+                    language,
+                    source_style, 
+                    target_style, 
+                    output_filepath, 
+                    synthesizer, 
+                    speaker_config,
+                    max_duration=max_duration,
+                    fadeout=fadeout,
+                    silence=silence):
+                    metadata.append(f"{output_filepath}|{text}|{speaker_config['language']}")
             except Exception as e:
-                print(f"Error processing with {speaker_config['name']}: {e}")
+                print(f"Error processing with {path_to_source_audio}: {e}")
                 continue
     
     with open(output_path / "train.txt", 'w', encoding='utf-8') as f:
@@ -117,6 +141,8 @@ def main():
     parser.add_argument("--speaker_config_path", type=str, required=True, help="Path to speaker config JSON file")
     parser.add_argument("--max_duration", type=float, default=None, help="Maximum duration in seconds for generated audio")
     parser.add_argument("--limit", type=int, default=None, help="Limit the number of samples generated per speaker")
+    parser.add_argument("--fadeout", type=float, default=0.1, help="Fadeout duration in seconds (default: 0.1)")
+    parser.add_argument("--silence", type=float, default=0.1, help="Silence duration in seconds to append (default: 0.1)")
     args = parser.parse_args()
     generate_speech_to_speech(
         model_config_path=args.model_config_path,
@@ -124,7 +150,9 @@ def main():
         output_path=args.output_path,
         speaker_config_path=args.speaker_config_path,
         max_duration=args.max_duration,
-        limit=args.limit
+        limit=args.limit,
+        fadeout=args.fadeout,
+        silence=args.silence
     )
 
 if __name__ == "__main__":
--- Scripts/run_generate_speech_to_speech.sh
+++ Scripts/run_generate_speech_to_speech.sh
@@ -1,17 +1,7 @@
-# s2s with sw syllables
-# python Scripts/generate_speech_to_speech.py \
-#     --model_config_path ./Models/SW-Bible-Victoria-20-epochs/config_ft_sw_bible_victoria.yml \
-#     --checkpoint_path ./Models/SW-Bible-Victoria-20-epochs/epoch_2nd_00019.pth \
-#     --reference_mode
-#     --output_path ../sw-Victoria-Althaf-24kHz \
-#     --speaker_config_path ./Scripts/Configs/Swahili/sentence.json \
-#     --max_duration 20.0 \
-#     --limit 10
-
 # EN-Althaf-S2S
 python Scripts/generate_speech_to_speech.py \
     --model_config_path ./Models/EN-Multi-ID-Althaf-emphasis/config_ft_en_multi_id_althaf_sw_victoria.yml \
     --checkpoint_path /home/s44504/3b01c699-3670-469b-801f-13880b9cac56/StyleTTS2/Models/EN-Multi-ID-Althaf-emphasis/epoch_2nd_00029.pth \
-    --output_path ../en-Multi-Althaf-Short-Sentence-24kHz \
-    --speaker_config_path ./Scripts/Configs/English/speech2speech/short_sentences.json \
+    --output_path ../en-Multi-Althaf-Exclamations-24kHz \
+    --speaker_config_path ./Scripts/Configs/English/speech2speech/exclamation.json \
     --max_duration 20.0
